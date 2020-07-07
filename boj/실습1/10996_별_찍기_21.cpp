1397e4bfb33f8b6d8bb4a69bcde9a8ac1e72fe5c
bhl0203 <59926426+bhl0203@users.noreply.github.com>
2020-07-07 23:04:53 +0900

    Add files via upload

---
+++ boj/실습1/10996_별_찍기_21.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d",&n);
+
+	if (n == 1) {
+		printf("*");
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j % 2 == 0) {
+					printf("*");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			printf("\n");
+			for (int j = 0; j < n; j++) {
+				if (j % 2 == 0) {
+					printf(" ");
+				}
+				else {
+					printf("*");
+				}
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
