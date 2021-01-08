7ff741862379061311b7d214b9bb0830cf76a570
bhl0203 <59926426+bhl0203@users.noreply.github.com>
2021-01-08 23:40:24 +0900

    SimplePractice

---
+++ boj/Practice.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n;
+    int res = 0;
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++) {
+        res += i;
+    }
+    printf("%d\n",res);
+    return 0;
+}
+
