0724903fce68450e02a4bbd47ba9ccc19aeb907a
bhl0203 <59926426+bhl0203@users.noreply.github.com>
2021-01-19 22:51:43 +0900

    TwoPointer/투포인터

---
+++ boj/2467_용액.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int arr[100001];
+
+int main() {
+	int n;
+
+	scanf("%d",&n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d",&arr[i]);
+	}
+	int l = 0;
+	int r = n - 1;
+	int res = 2000000000;
+	int tmp;
+	int resl, resr;
+	while (l < r) {
+		tmp = arr[l] + arr[r];
+		if (abs(tmp) < res) {
+			res = abs(tmp);
+			resl = l;
+			resr = r;
+		}
+		if (tmp > 0) r--;
+		else l++;
+	}
+	printf("%d %d\n",arr[resl],arr[resr]);
+	return 0;
+}
